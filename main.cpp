be2edb2305011f7df9c052079fe64dceae5e101c
laylahanyadel1915-rgb <laylahanyadel1915@gmail.com>
2025-11-12 15:22:03 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,10 +7,83 @@
 #include <iostream>
 using namespace std;
 
+class Animal{
 
-// ============== MAIN FUNCTION ==============
-int main() {
-    
-    
-    return 0;
+void Animal(){
+this->name= "";
+this->age= 0;
+this->isHungry= False;
 }
+void Animal(string name, int age, bool isHungry){
+this->name= name;
+this->age= age;
+this->isHungry= isHungry;
+}
+~Animal(){
+};
+void display(){
+cout<< name >> "(Age:" << age << isHungry();
+}
+
+void feed(){
+}
+};
+class Enclosure :: public Animal{
+  void  Enclosure(){
+        this->capacity = 0;
+        this->currentCount =0;
+
+    }
+
+  void  Enclosure(Animal* a , int capacity, int currentCount){
+     this->capacity = capacity;
+        this->currentCount =currentCount;
+    }
+    void addAnimal(Animal* a ){
+
+        }
+        void displayAnimals(){
+        cout<< Animal* a << endl;
+
+        }
+    ~Enclosure(){
+        
+    }
+        };
+
+class Visitor{
+
+    void Visitor(){
+    this->visitorName= "";
+    this-> ticketsBought= 0;
+    }
+
+    void Visitor(string visitorName, int ticketsBought){
+    this->visitorName= visitorName;
+    this-> ticketsBought= ticketsBought;
+    }
+
+
+    void displayInfo(){
+    cout<< "Visitor Info:" << endl;
+    cout << "Name:" << visitorName << endl;
+    cout << "Tickets Bought: " << ticketsBought << endl;
+    }
+~Vistor(){
+    
+}
+
+};
+
+ int main(){
+    Animal1= Animal(Lion ,5, isHungry(True));
+    Animal2= Animal(Parrot ,2, isHungry(False));
+    Animal3= Animal(Snake ,3,Venomous, isHungry(True));
+     Visitor1= Visitor("Sarah Ali", 3);
+     display(Animal1);
+     display(Animal2);
+     display(Animal3);
+     displayInfo(Visitor1);
+ return 0;}
+
+#endif // ENCLOSURE_H_INCLUDED
