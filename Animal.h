8a5e12fdc51a4dd4190dcf26ea9749d7f8bcad55
laylahanyadel1915-rgb <laylahanyadel1915@gmail.com>
2025-11-12 15:12:01 +0200

    Update Animal.h

--- Animal.h
+++ Animal.h
@@ -1,11 +1,41 @@
-#ifndef ANIMAL_H
-#define ANIMAL_H
-
+#ifndef ANIMAL_H_INCLUDED
+#define ANIMAL_H_INCLUDED
+#include <iostream>
 #include <string>
 using namespace std;
 
-//define animal class here 
+class Animal {
 
+private:
+    string name;
+    int age;
+    bool isHungry;
+public:
+    void display();
+    void feed();
+    ~Animal();
+void getName(){
+}
+void getAge(){
+}
+void isHungry(){
+    if (true){
+        cout<< "Hungry"<< endl;
+    }else cout << "Not Hungry" << endl:
+}
+if(name == "snake"){
+        Reptile-> True;
+}
+};
 
+class Mammal :: public Animal{
+private string furColor;
+};
+class Bird :: public Animal{
+private float wingSpan;
+};
+class Reptile :: public Animal{
+private bool isVenomous;
+};
 
-#endif
+#endif // ANIMAL_H_INCLUDED
